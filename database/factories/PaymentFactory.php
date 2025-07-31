fc8d10b50979c9302f9ffc930d66111968ea8746
thbappy7706 <tanvir15-7706@diu.edu.bd>
2025-08-01 00:29:24 +0600

    add tanstack datatable

---
+++ database/factories/PaymentFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Payment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Payment>
+ */
+class PaymentFactory extends Factory
+{
+    protected $model = Payment::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail(),
+            'amount' => $this->faker->randomFloat(2, 10, 1000),
+            'status' => $this->faker->randomElement(['pending', 'processing', 'success', 'failed']),
+        ];
+    }
+}
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -16,7 +16,8 @@ createInertiaApp({
         root.render(<App {...props} />);
     },
     progress: {
-        color: '#4B5563',
+        color: '#0ae50a',
+        showSpinner:true,
     },
 });
 
